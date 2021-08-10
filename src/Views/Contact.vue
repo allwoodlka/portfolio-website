3dc1af2e002030dae19c42f650ecb3bbca717dd2
Liam <allwoodlka@gmail.com>
2021-08-11 07:18:41 +1200

    Testing Netlify Forms

--- src/Views/Contact.vue
+++ src/Views/Contact.vue
@@ -5,18 +5,12 @@
   <br />
 
 
-  <form name="contact" method="POST" data-netlify="true">
+  <form name="contactform" method="POST" data-netlify="true">
     <p>
       <label>Your Name: <input type="text" name="name" /></label>
     </p>
     <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
+      <label>Your Email: <input type="text" name="email" /></label>
     </p>
     <p>
       <label>Message: <textarea name="message"></textarea></label>
