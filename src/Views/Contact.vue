9e105c01fd30b2c05932bcb8320e952a09143e98
Liam <allwoodlka@gmail.com>
2021-08-11 06:32:41 +1200

    Testing Netlify Forms

--- src/Views/Contact.vue
+++ src/Views/Contact.vue
@@ -3,6 +3,25 @@
   <p>If you want to get in touch with me, fill out this form with your information and I'll receive it in an email shortly after</p>
   <p>Don't forget to include a way for me to contact you back!</p>
   <br />
+
+  <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <div data-netlify-recaptcha="true"></div>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+
+
+
   <form ref="formref" name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
     <div class="p-fluid p-formgrid p-grid">
       <div class="p-field p-col-6">
