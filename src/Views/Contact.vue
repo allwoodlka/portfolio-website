47d44285ca96d8c6aff387518d8a2fefcc647a2a
Liam <allwoodlka@gmail.com>
2021-08-11 06:54:46 +1200

    Testing Netlify Forms

--- src/Views/Contact.vue
+++ src/Views/Contact.vue
@@ -4,26 +4,12 @@
   <p>Don't forget to include a way for me to contact you back!</p>
   <br />
 
-  <form name="contact" method="POST" data-netlify="true">
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
+  <form name="ask-question"
+        method="post" netlify netlify-honeypot="bot-field">
+    <input type="text" name="name" />
+    <input type="radio" name="panelist" />
+    <textarea name="question"></textarea>
+    <button type="submit">Send</button>
   </form>
 
 
