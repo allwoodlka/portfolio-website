6a82d378ddb17e13a5592618edba7736897990b4
Liam <allwoodlka@gmail.com>
2021-08-11 15:13:00 +1200

    Testing Netlify Forms.

--- src/Views/Contact.vue
+++ src/Views/Contact.vue
@@ -4,7 +4,7 @@
   <p>Don't forget to include a way for me to contact you back!</p>
   <br />
 
-  <form ref="formref" name="contactform" method="POST" id="contactForm" netlify>
+  <form ref="formref" name="contactform" method="POST" id="contactForm" netlify data-netlify-recaptcha="true">
     <input type="hidden" name="form-name" value="contactform" />
     <div class="p-fluid p-formgrid p-grid">
       <div class="p-field p-col-6">
