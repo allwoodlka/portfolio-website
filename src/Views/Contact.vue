d1ac299bc7595bc829077e5eb56e9f061f4ac2c0
Liam <allwoodlka@gmail.com>
2021-08-11 15:51:06 +1200

    Added simple form validation and honeypot

--- src/Views/Contact.vue
+++ src/Views/Contact.vue
@@ -9,18 +9,21 @@
     <div class="p-fluid p-formgrid p-grid">
       <div class="p-field p-col-6">
         <label for="name">Name</label>
-        <InputText id="name" type="text" v-model="nameVal" />
+        <InputText id="name" type="text" v-model="nameVal" :class="nameClass" />
         <input type="text" name="name" :value="nameVal" class="p-d-none" />
+        <span style="color: orangered" v-if="nameErrorMessage">{{ nameErrorMessage }}</span>
       </div>
       <div class="p-field p-col-6">
         <label for="contact">Preferred Contact</label>
-        <InputText id="contact" type="text" v-model="contactVal" />
+        <InputText id="contact" type="text" v-model="contactVal" :class="contactClass" />
         <input type="text" name="contact" :value="contactVal" class="p-d-none" />
+        <span style="color: orangered" v-if="contactErrorMessage">{{ contactErrorMessage }}</span>
       </div>
       <div class="p-field p-col-12">
         <label for="message">Message</label>
-        <Textarea id="message" rows="4" v-model="messageVal" />
-        <textarea name="message" :value="messageVal" class="p-d-none" />
+        <Textarea id="message" rows="6" v-model="messageVal" :class="messageClass" style="resize: vertical"/>
+        <textarea name="message" :value="messageVal" class="p-d-none"/>
+        <span style="color: orangered" v-if="messageErrorMessage">{{ messageErrorMessage }}</span>
       </div>
       <div class="p-col-6">
         <input name="liam" class="p-d-none" />
@@ -45,20 +48,54 @@ export default {
   },
   data(){
     return{
-      nameVal: null,
-      contactVal: null,
-      messageVal: null,
-
+      nameVal: "",
+      nameClass: "",
+      nameErrorMessage: false,
+      contactVal: "",
+      contactClass: "",
+      contactErrorMessage: false,
+      messageVal: "",
+      messageClass: "",
+      messageErrorMessage: false,
     }
   },
   methods: {
     submitForm(){
-      this.$refs.formref.submit();
+      if(this.validateForm()){
+        this.$refs.formref.submit();
+      }
+    },
+    validateForm(){
+      if(this.nameVal.length < 3){
+        this.nameClass = "p-invalid";
+        this.nameErrorMessage = "Name is too short.";
+      }else{
+        this.nameClass = "";
+        this.nameErrorMessage = false;
+      }
+      if(this.contactVal.length < 6){
+        this.contactClass = "p-invalid";
+        this.contactErrorMessage = "Contact method too short.";
+      }else{
+        this.contactClass = "";
+        this.contactErrorMessage = false;
+      }
+      if(this.messageVal.length < 10){
+        this.messageClass = "p-invalid";
+        this.messageErrorMessage = "Message too short.";
+      }else{
+        this.messageClass = "";
+        this.messageErrorMessage = false;
+      }
+      if(this.messageErrorMessage || this.contactErrorMessage || this.nameErrorMessage){
+        return false;
+      }else{
+        return true;
+      }
     }
   }
 }
 </script>
 
 <style>
-
 </style>
